a1b363fa8291dda079b6c03e69c7f6af2c87b369
Joonas Roosalu <joonas.roosalu@eesti.ee>
2022-06-28 16:08:15 +0300

    feat: #38 Add public read only API

---
+++ src/CentOps.Api/Controllers/PublicInstitutionController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CentOps.Api.Models;
+using CentOps.Api.Services.ModelStore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentOps.Api.Controllers
+{
+    [Route("public/institutions")]
+    [ApiController]
+    public class PublicInstitutionController : ControllerBase
+    {
+        private readonly IInstitutionStore _store;
+        private readonly IMapper _mapper;
+
+        public PublicInstitutionController(IInstitutionStore store, IMapper mapper)
+        {
+            _store = store;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<InstitutionResponseModel>>> Get()
+        {
+            var institutions = await _store.GetAll().ConfigureAwait(false);
+            return Ok(_mapper.Map<IEnumerable<InstitutionResponseModel>>(institutions));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<ParticipantResponseModel>>> Get(string id)
+        {
+            var instituion = await _store.GetById(id).ConfigureAwait(false);
+
+            if (instituion == null)
+            {
+                return NotFound(id);
+            }
+
+            var participants = await _store.GetParticipantsByInstitutionId(id).ConfigureAwait(false);
+
+            return Ok(_mapper.Map<IEnumerable<ParticipantResponseModel>>(participants));
+        }
+    }
+}
